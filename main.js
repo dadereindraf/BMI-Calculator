25fcba74520fd45d5ec2e035149d9692730f828f
Dade Reindra Firdaus <71720985+dadereindraf@users.noreply.github.com>
2023-04-04 08:42:34 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -4,28 +4,28 @@ const submitBtn = document.getElementById("submit-btn");
         hitungBMI();
     });
 
-    function hitungBMI() {
-    	// Mendapatkan nilai berat badan dan tinggi badan dari input pada form
-        const weight = document.getElementById("weight").value;
-        const height = document.getElementById("height").value;
+function hitungBMI() {
+    // Mendapatkan nilai berat badan dan tinggi badan dari input form
+    const weight = document.getElementById("weight").value;
+    const height = document.getElementById("height").value;
 
-        // Menghitung nilai BMI dengan rumus yang telah ditentukan
-        const bmi = weight / ((height / 100) ** 2);
+    //Rumus menghitung nilai BMI
+    const bmi = weight / ((height / 100) ** 2);
 
-        // Menampilkan hasil perhitungan BMI dan kategori BMI pada halaman web
-        const resultElement = document.getElementById("result");
-        resultElement.innerHTML = `Your BMI is <strong>${bmi.toFixed(1)}</strong> which means You are <strong>${getBMICategory(bmi)}</strong>`;
+    // Menampilkan hasil perhitungan BMI dan kategori BMI pada halaman web
+    const result = document.getElementById("result");
+    result.innerHTML = `Your BMI is <strong>${bmi.toFixed(1)}</strong> which means You are <strong>${BMICategory(bmi)}</strong>`;
+}
+
+// Function untuk menentukan kategori BMI berdasarkan nilai BMI
+function bmiCategory(bmi) {
+   	if (bmi < 18.5) {
+        return "Underweight";
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        return "Normal";
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        return "Overweight";
+    } else {
+        return "Obesity";
     }
-
-    // Function untuk menentukan kategori BMI berdasarkan nilai BMI
-    function getBMICategory(bmi) {
-    	if (bmi < 18.5) {
-          return "Underweight";
-        } else if (bmi >= 18.5 && bmi <= 24.9) {
-          return "Normal";
-        } else if (bmi >= 25 && bmi <= 29.9) {
-          return "Overweight";
-        } else {
-          return "Obesity";
-        }
-    }
+}
